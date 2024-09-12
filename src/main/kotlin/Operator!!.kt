fe5355f680cb7f3d7cfdcad9ed0c7291f5242d2c
GitaFebiola <gitaafebiolaa@gmail.com>
2024-09-12 14:54:46 +0700

    Operator!!.kt

--- src/main/kotlin/Operator!!.kt
+++ src/main/kotlin/Operator!!.kt
@@ -4,6 +4,6 @@ fun main(args: Array<String>) {
     var maybeNumber: Int? = 15
     println(maybeNumber!! * 2)
 
-    maybeNumber = null
-    println(maybeNumber!! * 2)
+//    maybeNumber = null
+//    println(maybeNumber!! * 2)
 }
